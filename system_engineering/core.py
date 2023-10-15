2973b6d3aab629460e40af11ff754a692e282ab3
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-10-15 21:11:55 +0900

    chore: Make it works for physical quantity

--- system_engineering/core.py
+++ system_engineering/core.py
@@ -1,10 +1,13 @@
 
-from typing import Any, Union, Tuple, Callable, TypeVar
+from dataclasses import dataclass, fields
+from typing import Any, Self, Union, Tuple, Callable, TypeVar
 from math import exp
-from webbrowser import Opera
-
+from scipy.optimize import minimize
 import numpy as np
 from pint import Quantity
+
+def get_magnitude(value: Union[Quantity, float]) -> float:
+    return value.magnitude if isinstance(value, Quantity) else value
 
 A = TypeVar("A")
 
@@ -17,8 +20,8 @@ class Eq:
 
     def __call__(self, vars: A) -> float:
         # If lhs is callable, call it with A; otherwise, use its value directly
-        lhs_value = self.lhs(vars) if callable(self.lhs) else self.lhs
-        rhs_value = self.rhs(vars) if callable(self.rhs) else self.rhs
+        lhs_value = get_magnitude(self.lhs(vars) if callable(self.lhs) else self.lhs)
+        rhs_value = get_magnitude(self.rhs(vars) if callable(self.rhs) else self.rhs)
         
         return exp(0.2 * abs(lhs_value - rhs_value)) - 1
     
@@ -31,8 +34,8 @@ class Lt:
 
     def __call__(self, vars: A) -> float:
         # If lhs is callable, call it with A; otherwise, use its value directly
-        lhs_value = self.lhs(vars) if callable(self.lhs) else self.lhs
-        rhs_value = self.rhs(vars) if callable(self.rhs) else self.rhs
+        lhs_value = get_magnitude(self.lhs(vars) if callable(self.lhs) else self.lhs)
+        rhs_value = get_magnitude(self.rhs(vars) if callable(self.rhs) else self.rhs)
         
         return exp(0.2 * abs(lhs_value - rhs_value)) - 1 if lhs_value > rhs_value else 0
     
@@ -45,8 +48,8 @@ class Gt:
 
     def __call__(self, vars: A) -> float:
         # If lhs is callable, call it with A; otherwise, use its value directly
-        lhs_value = self.lhs(vars) if callable(self.lhs) else self.lhs
-        rhs_value = self.rhs(vars) if callable(self.rhs) else self.rhs
+        lhs_value = get_magnitude(self.lhs(vars) if callable(self.lhs) else self.lhs)
+        rhs_value = get_magnitude(self.rhs(vars) if callable(self.rhs) else self.rhs)
         
         return exp(0.2 * abs(lhs_value - rhs_value)) - 1 if lhs_value < rhs_value else 0
         
@@ -57,3 +60,22 @@ class Mse:
     def __call__(self, vars: A) -> float:
         squared_loss = [f(vars) ** 2 for f in self.fs]
         return sum(squared_loss) / len(squared_loss)
+
+@dataclass
+class SystemParams:
+    def to_ndarray(self):
+        return np.array([get_magnitude(getattr(self, field.name)) for field in fields(self)]).flatten()
+    
+    def from_ndarray(self, values: np.ndarray) -> 'SystemParams':
+        quantities = {
+            field.name: 
+            (Quantity(value, getattr(self, field.name).units) if isinstance(getattr(self, field.name), Quantity) else value) for value, field in zip(values, fields(self))
+        }
+        return type(self)(**quantities)
+    
+    def solve(self, f: Callable[['SystemParams'], float]) -> 'SystemParams':
+        return self.from_ndarray(
+            minimize(
+                lambda x: f(self.from_ndarray(x)),
+                  self.to_ndarray(),
+                  tol=1e-9).x) 
--- test.py
+++ test.py
@@ -4,39 +4,24 @@ import numpy as np
 from pint import Quantity, UnitRegistry
 from scipy.optimize import minimize
 
-from system_engineering.core import Eq, Mse
+from system_engineering.core import Eq, Mse, SystemParams
 
-ureg = UnitRegistry
+ureg = UnitRegistry()
 Q = Quantity
 
 def get_magnitude(value: Union[Quantity, float]) -> float:
     return value.magnitude if isinstance(value, Quantity) else value
 
 @dataclass
-class PizzaDroneParam:
+class PizzaDroneParam(SystemParams):
     a: Q
     b: Q
+    
+initial = PizzaDroneParam(2 * ureg.meter, 0 * ureg.meter)
 
-    def to_ndarray(self):
-        return np.array([get_magnitude(getattr(self, field.name)) for field in fields(self)]).flatten()
-    
-    def from_ndarray(self, values: np.ndarray) -> Self:
-        quantities = {
-            field.name: 
-            (Q(value, getattr(self, field.name).units) if isinstance(getattr(self, field.name), Quantity) else value ) for value, field in zip(values, fields(self))}
-        return PizzaDroneParam(**quantities)
-    
-    def design(self, f: Callable[[Self], float]) -> Self:
-        return self.from_ndarray(
-            minimize(
-                lambda x: f(self.from_ndarray(x)),
-                  self.to_ndarray(),
-                  tol=1e-9).x)
-    
-initial = PizzaDroneParam(2, 0)
-
-res = initial.design(Mse(
-    Eq(lambda x: x.a , 2),
+res = initial.solve(
+    Mse(
+    Eq(lambda x: x.a , 2 * ureg.meter),
     Eq(lambda x: x.a + x.b, 42)
     ))
 
