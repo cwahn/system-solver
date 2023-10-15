e18a5b53d8e2f5b43f8718a6689cdb0781a36d4d
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-10-16 00:49:31 +0900

    chore: Amend example with better api

--- test.py
+++ test.py
@@ -1,26 +1,18 @@
 from dataclasses import dataclass
 from system_engineering.core import Eq, Gt, SystemParams, Q_
 
+# Specified values are initial value of numeric optimization
 @dataclass
-class PizzaDroneParam(SystemParams):
-    payload: Q_
-    frame_mass: Q_
-    pizza_box_mass: Q_
-    mtow: Q_
-    cruising_speed: Q_
-    endurance: Q_
-    range: Q_
-    
-initial = PizzaDroneParam(
-    payload= Q_(1, "kg"), 
-    frame_mass=  Q_(1, "kg"),
-    pizza_box_mass= Q_(0.5, "kg"),
-    mtow = Q_(4, "kg"),
-    cruising_speed = Q_(5, "m/s"),
-    endurance = Q_(10, "min"),
-    range = Q_(10, "km"))
+class PizzaDroneParams(SystemParams):
+    payload: Q_ = Q_(1, "kg")
+    frame_mass: Q_ =  Q_(1, "kg")
+    pizza_box_mass: Q_ = Q_(0.5, "kg")
+    mtow: Q_ = Q_(4, "kg")
+    cruising_speed: Q_ = Q_(5, "m/s")
+    endurance: Q_ = Q_(10, "min")
+    range: Q_ = Q_(10, "km")
 
-res, report = initial.solve(
+res, report = PizzaDroneParams().solve(
     Eq(lambda x: x.payload , Q_(1, "kg")),
     Eq(lambda x: x.mtow, lambda x: x.payload + x.frame_mass + x.pizza_box_mass),
     Gt(lambda x: x.frame_mass, Q_(0.5, "kg")),
